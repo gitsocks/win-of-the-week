d62242fc9d16f9e37bcbde20c1195de11908b6b2
Herco Bezuidenhout <herco.bezuidenhout@angloamerican.com>
2023-05-25 15:38:57 +0200

    Test out the .env change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,22 +2,27 @@ import { Heading, Text } from "@chakra-ui/react";
 
 export async function getStaticProps() {
   const helloWorld = process.env.HELLO_WORLD;
+  const anotherVariable = process.env.ANOTHER_VARIABLE;
+
   return {
     props: {
-      helloWorld
+      helloWorld,
+      anotherVariable
     }
   };
 }
 
 export interface HomePageProps {
   helloWorld: string;
+  anotherVariable: string;
 }
 
-const HomePage = ({ helloWorld }: HomePageProps) => {
+const HomePage = ({ helloWorld, anotherVariable }: HomePageProps) => {
   return (
     <>
       <Heading>Welcome to Win of the Week</Heading>
       <Text>{helloWorld || 'No helloWorld found'}</Text>
+      <Text>{anotherVariable || 'No anotherVariable found'}</Text>
     </>
   );
 };
