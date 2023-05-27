087361ccd0482125e6dd2205e70e9ce4b7ad6f3b
Herco Bezuidenhout <herco.bezuidenhout@angloamerican.com>
2023-05-27 14:00:32 +0200

    Fix up develop deployment

--- src/components/layouts/BaseLayout.tsx
+++ src/components/layouts/BaseLayout.tsx
@@ -1,8 +1,4 @@
-import { NotificationsProvider } from "@/providers/NotificationsProvider";
-import { ChakraProvider } from "@chakra-ui/react";
-import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren } from "react";
 
 export const BaseLayout = ({ children }: PropsWithChildren) => {
     return <>{children}</>;
