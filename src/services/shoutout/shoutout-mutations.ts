00cfa4c8b3c613a1796feab76fe1abaa9a651f72
Herco Bezuidenhout <herco.bezuidenhout@angloamerican.com>
2023-05-31 20:47:40 +0200

    Clean up shoutout mutations

--- src/services/shoutout/shoutout-mutations.ts
+++ src/services/shoutout/shoutout-mutations.ts
@@ -1,6 +1,5 @@
 import { useMutation } from "react-query";
 import { useShoutoutService } from "./shoutout-service";
-import { Shoutout } from "@prisma/client";
 import { CreateShoutout } from "@/types/CreateShoutout";
 
 export const useCreateShoutoutMutation = () => {
